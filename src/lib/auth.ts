5efa22879f2cfc3b4f1ddc8b3e997cf8bc7b9c9e
saturngod <saturngod@gmail.com>
2025-12-27 23:10:45 -0600

    setup better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+console.log("DB URL:", process.env.DATABASE_USER);
+const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5
+});
+console.log("Adapter setup Done");
+
+const prisma = new PrismaClient({ adapter });
+
+console.log("Prisma setup Done");
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mysql",
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
+});
+
